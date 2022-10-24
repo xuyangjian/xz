4202bcae44952ca7234b5536737fd77ac66b8916
xuyangjian <86253804+xuyangjian@users.noreply.github.com>
2022-10-24 18:59:37 +0800

    Update link.js

--- index_files/link.js
+++ index_files/link.js
@@ -2,12 +2,12 @@ var link = {
     //亚博
     yaboPc: 'https://kaiyun120.com:9517/entry/register/?i_code=71346184',                //亚博电脑
     yaboH5: 'https://kaiyun120.com:9517/entry/register/?i_code=71346184',          //亚博h5
-    yaboApp: 'https://www.ky135.app:9502/?i_code=71346184',                        //亚博aap
+    yaboApp: 'https://www.k25.app:9077/?i_code=71346184',                        //亚博aap
     
     //乐鱼
     leyuPc: 'https://kaiyun120.com:9517/entry/register/?i_code=71346184',                  //爱游戏电脑
     leyuH5: 'https://kaiyun120.com:9517/entry/register/?i_code=71346184',       //爱游戏h5 
-    leyuApp: 'https://www.ky135.app:9502/?i_code=71346184',                          //爱游戏app  
+    leyuApp: 'https://www.k25.app:9077/?i_code=71346184',                          //爱游戏app  
     
     //客服                                                        
     contact: 'http://hd66.tv/'                                
